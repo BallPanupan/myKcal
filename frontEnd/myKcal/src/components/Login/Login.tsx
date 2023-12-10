5e26699ee04b36714efdf99bf4ca83320e16ea9a
Panupan <Ball085573@hotmail.com>
2023-12-10 23:14:59 +0700

    update form login and interface

--- frontEnd/myKcal/src/components/Login/Login.tsx
+++ frontEnd/myKcal/src/components/Login/Login.tsx
@@ -1,47 +1,65 @@
-// src/components/Login.tsx
-import React, { useState } from 'react';
-import './Login.css';
+import React, { ChangeEvent } from 'react';
 
-const Login: React.FC = () => {
-  const [username, setUsername] = useState('Panupan');
-  const [password, setPassword] = useState('Password');
+interface LoginProps {
+	onLoginSuccess: (username: string, password: string) => void;
+}
 
-  const handleLogin = () => {
-    // Add your login logic here
-    console.log('Logging in with:', { username, password });
-  };
+const Login: React.FC<LoginProps> = ({ onLoginSuccess }) => {
+	const [username, setUsername] = React.useState<string>('Panupan');
+	const [password, setPassword] = React.useState<string>('Password');
 
-  return (
-    <div className="login-container">
-      <h1>myKcal</h1>
-      <div className='login'>
-      <h2>Login</h2>
-      <form>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="button" onClick={handleLogin} className="login-button">
-          Login
-        </button>
-      </form>
-      </div>
-    </div>
-  );
+	const handleLogin = () => {
+		// Add your login logic here
+		console.log('Logging in with:', { username, password });
+
+		// Call the callback on successful login
+		onLoginSuccess(username, password);
+	};
+
+	const handleUsernameChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setUsername(e.target.value);
+	};
+
+	const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setPassword(e.target.value);
+	};
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		handleLogin();
+	};
+
+	return (
+		<div className="login-container">
+			<h1>myKcal</h1>
+			<div className='login'>
+				<h2>Login</h2>
+				<form onSubmit={handleSubmit}>
+					<div className="form-group">
+						<label htmlFor="username">Username:</label>
+						<input
+							type="text"
+							id="username"
+							value={username}
+							onChange={handleUsernameChange}
+						/>
+					</div>
+					<div className="form-group">
+						<label htmlFor="password">Password:</label>
+						<input
+							type="password"
+							id="password"
+							value={password}
+							onChange={handlePasswordChange}
+						/>
+					</div>
+					<button type="submit" className="login-button">
+						Login
+					</button>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
